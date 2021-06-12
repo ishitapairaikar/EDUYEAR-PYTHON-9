14259b7c32d9acf117fbb99d19b52ce4154f0e45
ishitapairaikar <75527294+ishitapairaikar@users.noreply.github.com>
2021-06-12 21:19:32 +0800

    Add files via upload

---
+++ python_lesson_2.py
@@ -0,0 +1,17 @@
+#Create a small program
+#1. Take variables with values of different types
+#2. Print these in different lines and with appropriate messages (use .format())
+
+
+a = 1.15
+print(a, type(a))
+
+b = 9
+print(b, type(b))
+
+c = "Well done"
+print(c, type(c))
+
+print("He prepared for", a , "hours as he was feeling very confident with" , b , "years of training.", end=" ")
+print("The professor was amazed and congratulated him saying" ' "' , c , '!"')
+
